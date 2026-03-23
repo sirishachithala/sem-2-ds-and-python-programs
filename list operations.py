558a053d14ccb84a4de9435df5bf6567147ed363
sirishachithala <sirishachithala1266@gmail.com>
2026-03-23 16:03:49 +0530

    Add files via upload

---
+++ list operations.py
@@ -0,0 +1,9 @@
+a=[1,3,5,7]
+b=[2,4,8]
+print(a+b)
+print(a*2)
+print(a[2])
+print(a[-2])
+print(b[1:3])
+print(3 in a)
+print(3 not in a)
---
+++ spy number.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+sum_digits = 0
+product_digits = 1
+
+temp = n
+
+while temp > 0:
+    digit = temp % 10
+    sum_digits += digit
+    product_digits *= digit
+    temp //= 10
+
+if sum_digits == product_digits:
+    print("Spy Number")
+else:
+    print("Not Spy Number")
